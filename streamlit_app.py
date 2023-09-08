efb5a57d4b79530d2a282ea29da69bd18c81e8f8
maheshthettu7 <144412209+maheshthettu7@users.noreply.github.com>
2023-09-08 16:27:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,12 +21,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-streamlit.header("Fruityvice Fruit Advice")
+streamlit.header("Fruityvice Fruit Advice!")
 #request module to connect the web pages 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response)
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 
 # to load the json object 
