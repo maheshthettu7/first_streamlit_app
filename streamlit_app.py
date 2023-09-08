c00f26fb22946224009d94d34b969ff6226fe862
maheshthettu7 <144412209+maheshthettu7@users.noreply.github.com>
2023-09-08 16:24:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,12 @@ my_fruit_list.set_index('Fruit',inplace=True)
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-
+streamlit.header("Fruityvice Fruit Advice")
 #request module to connect the web pages 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response)
-# streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())
 
 
 # to load the json object 
