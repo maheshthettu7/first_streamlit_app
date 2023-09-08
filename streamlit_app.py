896629f21378eed515a7f189e754e79a1be2f4eb
maheshthettu7 <144412209+maheshthettu7@users.noreply.github.com>
2023-09-08 19:14:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas 
 import requests
-import urllib.error import URLError
+from urllib.error import URLError
 import snowflake.connector 
 streamlit.title("My Mom's New Healthy Dinner")
 
